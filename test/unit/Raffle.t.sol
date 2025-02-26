4bba24163cd058b7aea7d55a0d11ee1413350a1f
biswajit-debnath <biswajitdebnath405@gmail.com>
2025-02-26 20:05:17 +0530

    Test: More test cases added

--- test/unit/Raffle.t.sol
+++ test/unit/Raffle.t.sol
@@ -2,23 +2,30 @@
 
 pragma solidity ^0.8.28; 
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console, console2} from "forge-std/Test.sol";
+import {Vm} from "forge-std/Vm.sol";
 import {Raffle} from "../../src/Raffle.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol"; 
+
 
 contract RaffleTest is Test {
     Raffle raffle;
     HelperConfig helperConfig;
     DeployRaffle deployer;
+    HelperConfig.NetworkConfig networkConfig;
     uint256 constant ENTRANCE_FEE = 5e15;
-    address constant VRF_COORDINATOR_ADDRESS = 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B; // Sepolia testnet
+    // address constant VRF_COORDINATOR_ADDRESS = 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B; // Sepolia testnet
     address testUser = makeAddr("User");
+
+     event RandomNumberRequested(uint256 indexed reqId);
 
 
     function setUp() external {
         deployer = new DeployRaffle();
         (raffle, helperConfig) = deployer.run();
+        networkConfig = helperConfig.getConfig();
     }
 
 
@@ -150,8 +157,69 @@ contract RaffleTest is Test {
         raffle.runLottery();
     }
 
-    
+    function testIfEventEmittedAfterRunLotteryCalled() external {
+        uint256 amountToInvest = raffle.getEntranceFeeAmountInEth();
+        vm.deal(testUser, 1 ether);
+        vm.prank(testUser);
+        raffle.enterRaffle{value: amountToInvest}();
 
+        address testUser2 = makeAddr("TestUser2");
+        vm.deal(testUser2, 1 ether);
+        vm.prank(testUser2);
+        raffle.enterRaffle{value: amountToInvest}();
+
+       
+        vm.expectEmit(true, false, false, false, 1);
+        emit RandomNumberRequested(1);
+        raffle.runLottery();
+    }
+
+
+    function testReqIdEventEmittedAfterRunLotteryCalled() external {
+        uint256 amountToInvest = raffle.getEntranceFeeAmountInEth();
+        vm.deal(testUser, 1 ether);
+        vm.prank(testUser);
+        raffle.enterRaffle{value: amountToInvest}();
+
+        address testUser2 = makeAddr("TestUser2");
+        vm.deal(testUser2, 1 ether);
+        vm.prank(testUser2);
+        raffle.enterRaffle{value: amountToInvest}();
+
+        // call runLottery
+        vm.recordLogs();
+        uint256 reqId = raffle.runLottery();
+        // check if the event is emitted
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+        bytes32 requestedIdFromLog = entries[1].topics[1];
+
+        assert(uint256(requestedIdFromLog) == reqId );
+    }
+
+
+
+    /* _pickWinnerAndSendPrize Function testing */
+
+    function testWinnerSelectionLogicBasedOnRandomNumber() external {
+        uint256 amountToInvest = raffle.getEntranceFeeAmountInEth();
+        vm.deal(testUser, 1 ether);
+        vm.prank(testUser);
+        raffle.enterRaffle{value: amountToInvest}();
+
+        address testUser2 = makeAddr("TestUser2");
+        vm.deal(testUser2, 1 ether);
+        vm.prank(testUser2);
+        raffle.enterRaffle{value: amountToInvest}();
+
+        uint256 reqId = raffle.runLottery();
+        uint256 subIdFromRaffle = raffle.getSubscriptionId();
+
+        // console.log("Subsciption id::", networkConfig.subId);
+        // console.log("Subscription Id from rafle: ", subIdFromRaffle);
+        // Act as VRFCoordinator and call fulfillRandomWords
+        VRFCoordinatorV2_5Mock(networkConfig.vrfCoordinatorAddress).fulfillRandomWords(reqId, address(raffle));
+
+    }
 
 
     
